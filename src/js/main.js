2cd10131c8da26483d278a3d797be9b9b99aa735
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-13 19:13:35 +0900

    JS: add function scrollIntoView

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,31 @@
-"use strict";
 
+import Typing from './typing.js';
 (() => {
+    const messageArr = ["항상 성장하는", "기본이 충실한"];
+    const typing = new Typing(messageArr);
+    const headerElem = document.querySelector('#header');
 
-    const typingElem = document.querySelector('.typing');
-    const typingMessageArr = ["항상 성장하는", "기본에 충실한"];
-    let currentIdx = 0;
-    let typingIdx = 0;
-    let typingRafId;
-    let typingRafCnt = 0;
-    let typingStr = '';
-    let typingMode = 'typing';
-    let typingState = true;
-
-    function typing() {
-        typingRafCnt++;
-  
-        if(typingRafCnt % 15 === 0 && typingState) {
-            switch(typingMode) {
-                case "typing" : 
-                    const strArr = typingMessageArr[currentIdx].split('');
-                    let char = strArr[typingIdx++];
-                    if(char === ' '){
-                        const span = document.createElement('span');
-                        span.classList.add('space');
-                        typingElem.appendChild(span);
-                        typingStr = '';
-                    }
-
-                    if(document.querySelector('.space')) {
-                        typingStr = typingStr.concat(char);
-                        document.querySelector('.space').textContent = typingStr;
-                    }else {
-                        typingStr = typingStr.concat(char);
-                        typingElem.textContent = typingStr;
-                    }
-                    
-                    if(typingIdx === strArr.length) {
-                        if(currentIdx === typingMessageArr.length - 1) {
-                            cancelAnimationFrame(typingRafId);
-                            return;
-                        }
-                        typingIdx = 0;
-                        currentIdx++;
-                        typingState = false;
-                        setTimeout(() => {
-                            typingState = true;
-                            typingMode = 'delete';
-                        }, 1000);
-                    }
-                    break;
-                case "delete" :
-                    if(document.querySelector('.space')) {
-                        if(typingStr.length === 0) {
-                            typingElem.removeChild(document.querySelector('.space'));
-                        } else {
-                            typingStr = typingStr.slice(0, -1);
-                        document.querySelector('.space').textContent = typingStr;
-                        }
-                        
-                    }else {
-                        typingStr = typingElem.textContent.slice(0, -1);
-                        typingElem.textContent = typingStr;
-                        if(typingStr.length === 0) {
-                            typingMode = 'typing';
-                        }
-                    }
-    
-                    break;
-                default: throw new Error('Not valid typing mode');
-            }
-                
-            }
-            typingRafId = requestAnimationFrame(typing);
+    function headerClickHandler(e) {
+        let target = e.target;
+        console.log(target);
+        if(target.matches('.hamburger-menu')) {
+            headerElem.classList.toggle('show');
         }
 
+        if(target.tagName === 'A') {
+            scrollIntoView(target.dataset.category);
+        }
+    }
+
+    function scrollIntoView(selector) {
+        const scrollTo = document.querySelector(selector);
+        const topValue = scrollTo.offsetTop;
+        window.scrollTo({top: topValue, behavior: 'smooth'});
+    }
 
     function init() {
-        typing();
+        typing.play();
+        headerElem.addEventListener('click',headerClickHandler);
     }
 
     init();
