257e1558174751335b66ddcabede5e45825f4053
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-13 20:03:59 +0900

    JS: add scroll event to home content

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,11 @@ import Typing from './typing.js';
 (() => {
     const messageArr = ["항상 성장하는", "기본이 충실한"];
     const typing = new Typing(messageArr);
+    const homeContentElem = document.querySelector('#home .content');
     const headerElem = document.querySelector('#header');
+    const nextPageBtn = document.querySelector('.next-page');
+    let yOffset;
+
 
     function headerClickHandler(e) {
         let target = e.target;
@@ -26,6 +30,15 @@ import Typing from './typing.js';
     function init() {
         typing.play();
         headerElem.addEventListener('click',headerClickHandler);
+        nextPageBtn.addEventListener('click', function () {
+            scrollIntoView(this.dataset.category);
+        });
+        window.addEventListener('scroll', () => {
+            yOffset = window.pageYOffset;
+            const speed = homeContentElem.dataset.speed;
+            homeContentElem.style.transform = `translateY(${yOffset * speed}px)`;
+
+        });
     }
 
     init();
