91422805e21778813c9a2b6b303b49315798c9d4
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-14 19:23:47 +0900

    JS: add animation algorithm

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,130 @@ import Typing from './typing.js';
 (() => {
     const messageArr = ["항상 성장하는", "기본이 충실한"];
     const typing = new Typing(messageArr);
-    const homeContentElem = document.querySelector('#home .content');
     const mainElem = document.querySelector('#main');
     const headerElem = document.querySelector('#header');
     const nextPageBtn = document.querySelector('.next-page');
-    let yOffset;
+    const sceneInfo = [
+        {   
+            scrollHeight: 0,
+            objs: {
+                homeContentElem: document.querySelector('#home .content'),
+                skillColumnElem: document.querySelectorAll('.skill-column'),
+            },
+            values: {
+                skillColumn_opacity: [0, 1, {start: 0.65, end: 0.9}],
+                skillColumn_translateY:[1, 0, {start:0.65, end: 0.9}],
+            }
+        },
+        {   
+            scrollHeight: 0,
+            objs: {
 
+            },
+            values: {
+
+            }
+        },
+        {   
+            scrollHeight: 0,
+            objs: {
+
+            },
+            values: {
+
+            }
+        },
+    ]
+    let yOffset;
+    let prevScrollHeight = 0;
+    let currentScene = 0;
+    let enterNewScene = false;
+
+    function setLayout() {
+        const mainSectionList = Array.from(mainElem.children)
+                                .filter(section => section.id !== 'header');
+        for(const idx in mainSectionList) {
+            sceneInfo[idx].scrollHeight = mainSectionList[idx].offsetHeight;
+        }
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+        for(let i=0; i<sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if(totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+    }
+    
+    function scrollLoop() {
+        enterNewScene = false;
+        prevScrollHeight = 0;
+        for(let i=0; i<currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
+            if(currentScene < sceneInfo.length - 1) {
+                currentScene++;
+            }
+        }
+
+        if(yOffset < prevScrollHeight) {
+            enterNewScene = true;
+            if(currentScene === 0) return;
+            currentScene--;
+        }
+
+        if(enterNewScene) return;
+
+        playAnimation();
+    }
+    
+    function playAnimation() {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+
+        switch(currentScene) {
+            case 0: 
+                const speed = objs.homeContentElem.dataset.speed;
+                objs.homeContentElem.style.transform = `translateY(${yOffset * speed}px)`;
+                const columnList = Array.from(objs.skillColumnElem);
+                for(const idx in columnList) {
+                    columnList[idx].style.opacity = `${calcValues(values.skillColumn_opacity, currentYOffset)}`;
+                    columnList[idx].style.transform = `translateY(${calcValues(values.skillColumn_translateY, currentYOffset) * 100 }px)`;
+                };
+                
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+
+    }
+
+    function calcValues(values, currentYOffset) {
+        let result;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const partScrollStart = values[2].start * scrollHeight;
+        const partScrollEnd = values[2].end * scrollHeight;
+        const partScrollHeight = partScrollEnd - partScrollStart;
+
+        if(currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+            result = ((currentYOffset - partScrollStart) / partScrollHeight) * (values[1] - values[0]) + values[0];
+        } else if(currentYOffset < partScrollStart) {
+            result = values[0];
+        } else if(currentYOffset > partScrollEnd) {
+            result = values[1];
+        }
+
+        return result;
+    }
 
     function headerClickHandler(e) {
         let target = e.target;
@@ -48,15 +166,16 @@ import Typing from './typing.js';
         });
         window.addEventListener('scroll', () => {
             yOffset = window.pageYOffset;
-            const speed = homeContentElem.dataset.speed;
-            homeContentElem.style.transform = `translateY(${yOffset * speed}px)`;
+            console.log(yOffset);
+            scrollLoop();
             headerFixHandler();
         });
         window.addEventListener('load', () => {
             yOffset = window.pageYOffset;
             headerFixHandler();
+            setLayout();
         });
     }
-
+    console.log(document.body.offsetHeight - innerHeight);
     init();
 })();
