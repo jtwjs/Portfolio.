b53f48a6ffe048a89cc475dbb08f590740c0f80a
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-14 17:36:31 +0900

    JS: add header scroll fixed

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import Typing from './typing.js';
     const messageArr = ["항상 성장하는", "기본이 충실한"];
     const typing = new Typing(messageArr);
     const homeContentElem = document.querySelector('#home .content');
+    const mainElem = document.querySelector('#main');
     const headerElem = document.querySelector('#header');
     const nextPageBtn = document.querySelector('.next-page');
     let yOffset;
@@ -14,17 +15,29 @@ import Typing from './typing.js';
         console.log(target);
         if(target.matches('.hamburger-menu')) {
             headerElem.classList.toggle('show');
-        }
+            }
 
         if(target.tagName === 'A') {
             scrollIntoView(target.dataset.category);
+            headerElem.classList.remove('show');
         }
     }
 
     function scrollIntoView(selector) {
         const scrollTo = document.querySelector(selector);
-        const topValue = scrollTo.offsetTop;
+        let topValue = scrollTo.offsetTop;
+        if(scrollTo.offsetParent) {
+            topValue += scrollTo.offsetParent.offsetTop;
+        }
         window.scrollTo({top: topValue, behavior: 'smooth'});
+    }
+
+    function headerFixHandler() {
+        if(yOffset >= innerHeight) {
+            mainElem.classList.add('fixed');
+        } else {
+            mainElem.classList.remove('fixed');
+        }
     }
 
     function init() {
@@ -37,7 +50,11 @@ import Typing from './typing.js';
             yOffset = window.pageYOffset;
             const speed = homeContentElem.dataset.speed;
             homeContentElem.style.transform = `translateY(${yOffset * speed}px)`;
-
+            headerFixHandler();
+        });
+        window.addEventListener('load', () => {
+            yOffset = window.pageYOffset;
+            headerFixHandler();
         });
     }
 
